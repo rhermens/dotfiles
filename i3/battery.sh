19a69ba1598887a1fffc60a74fb366261f487a73
Roy <royhermens@hotmail.com>
2022-11-28 08:17:55 +0100

    Wallpapers, bt name

--- i3/battery.sh
+++ i3/battery.sh
@@ -2,15 +2,14 @@
 
 percent=$(bluetoothctl info $1 | grep "Battery Percentage" | cut -d ' ' -f4 | sed 's/^.//;s/.$//')
 connected=$(bluetoothctl info $1 | grep "Connected" | cut -d ' ' -f2)
+name=$(bluetoothctl info $1 | grep "Name" | cut -d ':' -f2)
 
 if [[ $connected == "yes" ]]; then
     if [ -z "$percent" ]; then
         echo "?"
     else
-        echo "Mouse: $percent%"
+        echo "$name: $percent%"
     fi
 elif [[ $connected == "no" ]]; then
     echo "off"
-else
-    echo "Disconnected"
 fi
