4fe91cf60a0dfc68bb7f903534a99d40211399c6
Roy <royhermens@hotmail.com>
2022-12-26 12:02:56 +0100

    Fd alt c

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,7 +1,9 @@
 if [[ -e /usr/share/fzf/key-bindings.zsh ]]; then
     if [[ -e /usr/bin/rg ]]; then
         export FZF_DEFAULT_COMMAND="rg --files --no-ignore-vcs --hidden -g !node_modules -g !vendor -g !.git"
-        export FZF_ALT_C_COMMAND=$FZF_DEFAULT_COMMAND
+    fi
+    if [[ -e /usr/bin/fd ]]; then
+        export FZF_ALT_C_COMMAND="fd --type d --hidden --follow --exclude node_modules --exclude vendor --exclude .git"
     fi
     source /usr/share/fzf/key-bindings.zsh
     source /usr/share/fzf/completion.zsh
