3b40f88323449adf02bcdb56d118491a2dd67685
Roy <royhermens@hotmail.com>
2023-10-24 07:38:23 +0200

    Fix move block up

--- nvim/lua/usermod/keymaps.lua
+++ nvim/lua/usermod/keymaps.lua
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<C-j>', ':bprev<CR>')
 vim.keymap.set('n', '<A-j>', ':m .+1 <CR>==')
 vim.keymap.set('n', '<A-k>', ':m .-2 <CR>==')
 vim.keymap.set('v', '<A-j>', ':m \'>+1 <CR>gv=gv')
-vim.keymap.set('v', '<A-k>', ':m \'>-2 <CR>gv=gv')
+vim.keymap.set('v', '<A-k>', ':m-2 <CR>gv=gv')
 
 vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = '*',
