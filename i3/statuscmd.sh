e3c1198dbf74edd3eb66922892a03bbaeecedd17
Roy <royhermens@hotmail.com>
2025-07-05 13:31:10 +0200

    cleanup

--- i3/statuscmd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-i3status | while :
-do
-        read line
-        echo "$(timer current) $(~/.config/i3/battery.sh)     $line   " || exit 1
-done
--- i3/volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-msgTag="volumeNotification"
-
-newvolume="$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | grep -Eo '[0-9]+' )"
-
-dunstify -a "Volume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
-    -h int:value:"$newvolume" "Vol:" 
