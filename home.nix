94e5f157f988670e60cd987063788ebffc97e680
Roy <royhermens@hotmail.com>
2025-07-05 13:29:38 +0200

    asdf-vm & dunst

--- home.nix
+++ home.nix
@@ -32,6 +32,11 @@
       source = ./tmux;
       target = ".config/tmux";
     };
+    dunst = {
+      enable = true;
+      source = ./dunst;
+      target = ".config/dunst";
+    };
   };
 
   # The home.packages option allows you to install Nix packages into your
@@ -50,7 +55,7 @@
 
     pkgs.nerd-fonts.iosevka
 
-    pkgs.asdf
+    pkgs.asdf-vm
     pkgs.go
 
     pkgs.uwsm
@@ -154,21 +159,6 @@
     };
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
@@ -212,6 +202,9 @@
   };
 
   services.hyprpolkitagent = {
+    enable = true;
+  };
+  services.dunst = {
     enable = true;
   };
 }
