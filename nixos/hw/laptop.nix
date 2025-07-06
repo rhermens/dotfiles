86ee00465f83a61767016127714a16cf1c6cbd66
rhermens <royhermens@hotmail.com>
2025-07-06 09:20:35 +0200

    Laptop gnome

--- nixos/hw/laptop.nix
+++ nixos/hw/laptop.nix
@@ -1,6 +1,13 @@
-{ ..., pkgs }:
+{ pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
+
+  services.desktopManager.gnome = {
+    enable = true;
+  };
+  services.displayManager.gdm = {
+    enable = true;
+  };
 }
