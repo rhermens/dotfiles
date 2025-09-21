a0d404fd6e7c6db41341abf54a46786d6daa9cc9
rhermens <royhermens@hotmail.com>
2025-09-21 16:43:31 +0200

    wofi style

--- install/programs.sh
+++ install/programs.sh
@@ -1,4 +1,4 @@
 sudo pacman -S --needed --noconfirm obsidian vlc libreoffice-fresh gedit transmission-gtk \
-    discord
+    discord vlc-plugin-ffmpeg
 
 yay -S --noconfirm --needed google-chrome slack-desktop
