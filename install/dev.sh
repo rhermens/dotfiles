8a29a00fc1fdee01091bf9a06dad20047371009f
rhermens <royhermens@hotmail.com>
2026-01-21 12:14:54 +0100

    asdf -> mise

--- install/dev.sh
+++ install/dev.sh
@@ -1,26 +1,10 @@
-sudo pacman -S --needed --noconfirm neovim act
-
-yay -S --noconfirm --needed asdf-vm
+sudo pacman -S --needed --noconfirm neovim act mise
 
 if [ -z "${WSL_DISTRO_NAME}" ]; then
 	yay -S --noconfirm mongodb-compass-bin postman-bin
 fi
 
-asdf plugin add nodejs
-asdf plugin add pnpm
-asdf plugin add uv
-asdf plugin add golang
-asdf plugin add rust
-
-asdf install golang latest
-asdf install rust latest
-asdf install nodejs latest
-asdf install pnpm latest
-
-asdf set --home golang latest
-asdf set --home rust latest
-asdf set --home nodejs latest
-asdf set --home pnpm latest
+mise install
 
 stow nvim
 stow dev
