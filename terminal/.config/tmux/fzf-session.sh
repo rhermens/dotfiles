d32edee92bab383c5e43368cc0cf2a4388cc5798
rhermens <royhermens@hotmail.com>
2026-03-02 07:22:18 +0100

    new windows

--- terminal/.config/tmux/fzf-session.sh
+++ terminal/.config/tmux/fzf-session.sh
@@ -4,7 +4,7 @@ selected=$(find ~ ~/Code ~/Werk -mindepth 1 -maxdepth 1 -type d | fzf --tmux)
 selected_name=$(basename "$selected" | tr . _)
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
+    tmux new-session -ds "$selected_name" -c "$selected" \; new-window -c "$selected" \; new-window -c "$selected" \; new-window -c "$selected" \; new-window -c "$selected"
 fi
 
 tmux switch-client -t "$selected_name"
