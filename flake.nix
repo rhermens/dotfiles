85c820fc62b4b13e7f3417258edd24a256e48a3d
Roy Hermens <roy@debitroom.com>
2026-05-29 08:11:08 +0200

    nix cfg

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      omen = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nix/hardware-configuration.nix
+          ./nix/configuration-omen.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.roy = ./nix/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ nix/home.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, ... }:
+{
+  home.username = "roy";
+  home.stateVersion = "26.05";
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.gcc
+    pkgs.rustup
+
+    pkgs.tmux
+    pkgs.ghostty
+    pkgs.tree-sitter
+    pkgs.neovim
+    pkgs.zsh
+    pkgs.antidote
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.tldr
+
+    pkgs._1password-gui
+    pkgs._1password-cli
+    pkgs.obsidian
+    pkgs.google-chrome
+    pkgs.discord
+    pkgs.slack-desktop
+    pkgs.iosevka
+
+    pkgs.claude-code
+    pkgs.mise
+    pkgs.mongodb-compass
+    pkgs.gh
+    pkgs.docker
+    pkgs.docker-compose
+  ];
+
+
+  home.file = {
+    ".config/opencode".source = config.lib.file.mkOutOfStoreSymlink ../ai/.config/opencode;
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../nvim/.config/nvim;
+    ".config/tmux".source = config.lib.file.mkOutOfStoreSymlink ../terminal/.config/tmux;
+    ".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink ../terminal/.config/ghostty;
+    ".config/git".source = config.lib.file.mkOutOfStoreSymlink ../core/.config/git;
+  };
+
+  programs.zsh = {
+    enable = true;
+    plugins = [
+      {
+        name = "p10k";
+        file = "p10k.zsh";
+        src = ../terminal/.zsh/plugins;
+      }
+    ];
+    antidote = {
+      enable = true;
+      plugins = [
+        "romkatv/powerlevel10k"
+        "zsh-users/zsh-syntax-highlighting"
+        "zsh-users/zsh-autosuggestions"
+        "jeffreytse/zsh-vi-mode"
+      ];
+    };
+  };
+}
