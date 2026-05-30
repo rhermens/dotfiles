b835ace4918e5d725611888aaa39f7aea113eac4
Roy Hermens <roy@debitroom.com>
2026-05-30 10:52:46 +0200

    pin home-manager to unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hp-tracerled.url = "github:rhermens/hp-tracerled-rs";
