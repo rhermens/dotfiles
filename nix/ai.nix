2109887444d171f3b3eade02457779ae74f6fdc1
Roy Hermens <roy@debitroom.com>
2026-07-14 07:55:16 +0200

    bumps

--- nix/ai.nix
+++ nix/ai.nix
@@ -5,12 +5,12 @@ let
   hermesDesktopMacLauncher = pkgs.writeShellScript "hermes-desktop-mac-launcher" ''
     exec ${hermesDesktop}/bin/hermes-desktop "$@"
   '';
-  hermesDesktopIcon = pkgs.runCommand "hermes-desktop.icns"
-    {
-      nativeBuildInputs = [ pkgs.libicns ];
-    } ''
-    png2icns $out ${hermesDesktop}/share/icons/hicolor/512x512/apps/hermes-desktop.png
-  '';
+  # hermesDesktopIcon = pkgs.runCommand "hermes-desktop.icns"
+  #   {
+  #     nativeBuildInputs = [ pkgs.libicns ];
+  #   } ''
+  #   png2icns $out ${hermesDesktop}/share/icons/hicolor/512x512/apps/hermes-desktop.png
+  # '';
 in
 {
   home.packages = [
@@ -27,7 +27,7 @@ in
   } // lib.optionalAttrs pkgs.stdenv.isDarwin {
     "Applications/Hermes Desktop.app/Contents/Info.plist".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/ai/hermes/Hermes.plist";
     "Applications/Hermes Desktop.app/Contents/MacOS/hermes-desktop".source = hermesDesktopMacLauncher;
-    "Applications/Hermes Desktop.app/Contents/Resources/hermes-desktop.icns".source = hermesDesktopIcon;
+    # "Applications/Hermes Desktop.app/Contents/Resources/hermes-desktop.icns".source = hermesDesktopIcon;
   };
 
   home.sessionPath = [
